d640f2c71ccbaec7bb2321aa08ae8c93be2a4328
Aleksei Kudinov <reanim86@yandex.ru>
2022-08-17 17:59:45 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+with open('1.txt', encoding='utf-8') as file1:
+    f1 = file1.readlines()
+
+with open('2.txt', encoding='utf-8') as file2:
+    f2 = file2.readlines()
+
+with open('3.txt', encoding='utf-8') as file3:
+    f3 = file3.readlines()
+
+print(f3)
