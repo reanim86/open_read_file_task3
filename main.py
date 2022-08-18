ed83b6200bc6387c1af672ae31b6e2b9e7414ed0
Aleksei Kudinov <reanim86@yandex.ru>
2022-08-18 16:00:23 +0300

    Add open_file

--- main.py
+++ main.py
@@ -1,34 +1,26 @@
 import os.path
 
-with open('1.txt', encoding='utf-8') as file1:
-    f1 = file1.readlines()
-    f1_name = os.path.basename('1.txt')
-    res_f1 = [f'{f1_name}\n', f'{str(len(f1))}\n']
-    res_f1.extend(f1)
-    res_f1.append('\n')
-
-
-with open('2.txt', encoding='utf-8') as file2:
-    f2 = file2.readlines()
-    f2_name = os.path.basename('2.txt')
-    res_f2 = [f'{f2_name}\n', f'{str(len(f2))}\n']
-    res_f2.extend(f2)
-    res_f2.append('\n')
-
-with open('3.txt', encoding='utf-8') as file3:
-    f3 = file3.readlines()
-    f3_name = os.path.basename('3.txt')
-    res_f3 = [f'{f3_name}\n', f'{str(len(f3))}\n']
-    res_f3.extend(f3)
-    res_f3.append('\n')
+def open_file(file):
+    with open(file, encoding='utf-8') as file_open:
+        f = file_open.readlines()
+        f_name = os.path.basename(file)
+        res_f = [f'{f_name}\n', f'{str(len(f))}\n']
+        res_f.extend(f)
+        res_f.append('\n')
+    return res_f
 
 def add_info(file):
     with open('res.txt', 'a', encoding='utf-8') as res:
         res.writelines(file)
     return
 
-file_list = [res_f1, res_f2, res_f3]
+file_list = []
+base_file = os.listdir('./base_file')
+
+for file in base_file:
+    file_list.append(open_file(f'./base_file/{file}'))
 file_list.sort(key=len)
+
 for add_file in file_list:
-    add_info(add_file)
+     add_info(add_file)
 
